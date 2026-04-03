0fbf48f9de30d93fec86a3ad72f2568664808690
lodhiji1255-cloud <lodhiji1255@gmail.com>
2026-04-04 00:20:29 +0530

    mapping and event crreation

--- contracts/managedelection.sol
+++ contracts/managedelection.sol
@@ -7,4 +7,22 @@ contract ManageElection {
         string name;
         uint voteCount;
     }
-}
+     mapping(uint => Candidate) public candidates;
+    uint public candidateCount;
+
+    // enrollment -> has voted?
+    mapping(string => bool) public hasVoted;
+
+    // faceHash (bytes32) -> used for voting?
+    mapping(bytes32 => bool) public usedFace;
+
+    // faceHash -> registered?
+    mapping(bytes32 => bool) public registeredFace;
+
+    // enrollment -> registered?
+    mapping(string => bool) public registeredEnrollment;
+    // event for blockchain alert
+    event CandidateAdded(uint id, string name);
+    event VoterRegistered(string enrollment, bytes32 faceHash);
+    event Voted(string enrollment, bytes32 faceHash, uint candidateId);
+} 
