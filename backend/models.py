e38762a738ecf55ddc340fcd17f5bd5d0a5a2c6a
shaktiankit9-hash <shaktiankit9@gmail.com>
2026-04-04 08:17:44 +0530

    Creation of tables in mysql database

--- backend/models.py
+++ backend/models.py
@@ -26,3 +26,26 @@ DATABASE_URL = (
 # ---------------------------------------------------------
 engine = create_engine(DATABASE_URL, echo=False)
 Base = declarative_base()
+# ---------------------------------------------------------
+#   MODELS
+# ---------------------------------------------------------
+class Admin(Base):
+    _tablename_ = "admin"
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    username = Column(String(100), unique=True, nullable=False)
+    password_hash = Column(String(300), nullable=False)
+
+    # Face embedding (128 floats ≈ 512 bytes, but we keep LONGBLOB for future)
+    face_encoding = Column(LONGBLOB, nullable=False)
+
+
+class Voter(Base):
+    _tablename_ = "voters"
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    enrollment = Column(String(50), unique=True, nullable=False)
+    name = Column(String(100), nullable=False)
+
+    # Face embedding
+    face_encoding = Column(LONGBLOB, nullable=False)
